114a133cae2b30d7c4c7631adc58bc2d09bb9237
SajidaDahir <sajidadahirsheikh@gmail.com>
2025-09-12 14:05:45 +0300

    repo name change

--- testMail.php
+++ testMail.php
@@ -19,4 +19,23 @@ $mailCont = [
             <br>Welcome to our application! This is a new semester"
 ];
 
+// connect using PDO
+try {
+    $dsn = "mysql:host={$conf['db_host']};dbname={$conf['db_name']};charset=utf8";
+    $pdo = new PDO($dsn, $conf['db_user'], $conf['db_pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // insert user
+    $stmt = $pdo->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+    $stmt->execute([
+        $username,
+        $email,
+        password_hash($_POST['password'], PASSWORD_DEFAULT) // always hash passwords
+    ]);
+
+} catch (PDOException $e) {
+    die("DB error: " . $e->getMessage());
+}
+
+
 $ObjSendMail->sendMail($conf, $mailCont);
